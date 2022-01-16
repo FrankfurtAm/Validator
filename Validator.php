22241b560da75750738b624c157ea0286d668b01
FrankfurtAm <bobkov-03@mail.ru>
2022-01-17 00:41:02 +0600

    Validator

--- Validator.php
+++ Validator.php
@@ -3,9 +3,14 @@
 class Validator
 {
 
-    public static function is_email(string $email)
+    public static function isEmail(string $email)
     {
         return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+    public static function verifyPasswords(string $password1, string $password2)
+    {
+        return $password1 === $password2; 
     }
 
     public static function isNotLoggedIn()
@@ -23,8 +28,4 @@ class Validator
         return ($id === (int)$_SESSION['authorizedUser']['id'])? true : false;
     }
 
-    public static function verifyPasswords(string $password1, string $password2)
-    {
-        return $password1 === $password2; 
-    }
 }
